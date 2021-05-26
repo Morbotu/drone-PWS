36d89784a21a5c91eefda995930dbd5cda184cf5
Morbotu <romboutlambert@gmail.com>
2021-05-26 14:16:07 +0200

    Beetje opruimen

--- live_camera_mac.py
+++ live_camera_mac.py
@@ -6,7 +6,7 @@ Dit programma zet de mac camera aan om te filmen en laat het op het scherm zien.
 Door Rombout Jansen
 """
 
-
+# Modules
 import os
 import numpy as np
 import PIL.Image as pil
@@ -16,35 +16,44 @@ import torch
 from torchvision import transforms
 import cv2
 
+# Geinstalleerde bestanden
 import networks
 from utils import download_model_if_doesnt_exist
 
 
-def display_depth() -> None:
-    """Functie die de met de camera filmt, het beeld omzet in diepte en een preview ervan laat zien.
-    """
+def main() -> None:
+    display_depth()
 
-    # De naam van het model
-    model_name = "mono+stereo_640x192"
 
+def get_device() -> torch.device:
     if torch.cuda.is_available():
         device = torch.device("cuda")
     else:
         device = torch.device("cpu")
+    return device
 
+
+def display_depth(model_name: str = "mono+stereo_640x192") -> None:
+    """Functie die de met de camera filmt, het beeld omzet in diepte en een preview ervan laat zien.
+    """
+
+    device = get_device()
+
+    # Download het model
     download_model_if_doesnt_exist(model_name)
     model_path = os.path.join("models", model_name)
     encoder_path = os.path.join(model_path, "encoder.pth")
     depth_decoder_path = os.path.join(model_path, "depth.pth")
 
-    # LOADING PRETRAINED MODEL
+    # Laat getrainde model
     encoder = networks.ResnetEncoder(18, False)
     loaded_dict_enc = torch.load(encoder_path, map_location=device)
 
     # extract the height and width of image that this model was trained with
     feed_height = loaded_dict_enc['height']
     feed_width = loaded_dict_enc['width']
-    filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
+    filtered_dict_enc = {
+        k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
     encoder.load_state_dict(filtered_dict_enc)
     encoder.to(device)
     encoder.eval()
@@ -65,7 +74,7 @@ def display_depth() -> None:
         cv2.namedWindow("normal")
         vc = cv2.VideoCapture(0)
 
-        if vc.isOpened(): # try to get the first frame
+        if vc.isOpened():  # try to get the first frame
             rval, frame = vc.read()
             frame2 = frame
         else:
@@ -95,18 +104,20 @@ def display_depth() -> None:
             # Saving colormapped depth image
             disp_resized_np = disp_resized.squeeze().cpu().numpy()
             vmax = np.percentile(disp_resized_np, 95)
-            normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(), vmax=vmax)
+            normalizer = mpl.colors.Normalize(
+                vmin=disp_resized_np.min(), vmax=vmax)
             mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
-            colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
+            colormapped_im = (mapper.to_rgba(disp_resized_np)[
+                              :, :, :3] * 255).astype(np.uint8)
             frame = pil.fromarray(colormapped_im)
             frame = np.asarray(frame)
 
             key = cv2.waitKey(20)
-            if key == 27: # exit on ESC
+            if key == 27:  # exit on ESC
                 break
 
         cv2.destroyWindow("preview")
 
 
 if __name__ == "__main__":
-    display_depth()
+    main()

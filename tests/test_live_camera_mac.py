7f07b26720a1965d40475b24b09f15c558e59d46
Morbotu <romboutlambert@gmail.com>
2021-06-05 20:01:39 +0200

    Voeg dingen toe aan tests

--- tests/test_live_camera_mac.py
+++ tests/test_live_camera_mac.py
@@ -10,3 +10,17 @@ class TestDownloadModel:
     def test_depth_decoder_path(self):
         assert live_camera_mac.download_model(
             "mono+stereo_640x192")[1] == "src/live_camera_mac/models/mono+stereo_640x192/depth.pth"
+
+
+@pytest.fixture
+def device():
+    return live_camera_mac.get_device()
+
+
+@pytest.fixture
+def encoder_path():
+    return live_camera_mac.download_model("mono+stereo_640x192")[0]
+
+
+def test_load_trained_model(encoder_path, device):
+    live_camera_mac.load_trained_model(encoder_path, device)

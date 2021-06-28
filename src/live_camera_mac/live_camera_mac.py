6c5446e7ae264f08ad314d655daefd0fefd1fb9c
Morbotu <romboutlambert@gmail.com>
2021-06-28 12:01:09 +0200

    Create better docstrings

--- src/live_camera_mac/live_camera_mac.py
+++ src/live_camera_mac/live_camera_mac.py
@@ -26,7 +26,10 @@ import networks
 
 
 def get_device() -> torch.device:
-    """Opens a device.
+    """Opens a device
+
+    Returns:
+        torch.device: The device used to analyze the pictures
     """
     if torch.cuda.is_available():
         device = torch.device("cuda")
@@ -36,7 +39,13 @@ def get_device() -> torch.device:
 
 
 def download_model(model_name: str) -> tuple[str, str]:
-    """Downloads the model.
+    """Downloads the model
+
+    Args:
+        model_name (str): The name of the model
+
+    Returns:
+        tuple[str, str]: The paths to the depth decoder and the encoder
     """
     download_model_if_doesnt_exist(model_name)
     model_path = os.path.join("src/live_camera_mac/models", model_name)
@@ -47,7 +56,14 @@ def download_model(model_name: str) -> tuple[str, str]:
 
 def load_trained_model(encoder_path: str,
                        device: torch.device) -> tuple[networks.ResnetEncoder, dict]:
-    """Load the trained model.
+    """Load the trained model
+
+    Args:
+        encoder_path (str): The path to the encoder
+        device (torch.device): The device 
+
+    Returns:
+        tuple[networks.ResnetEncoder, dict]: The ResnetEncoder and the data
     """
     encoder = networks.ResnetEncoder(18, False)
     loaded_dict_enc = torch.load(encoder_path, map_location=device)
@@ -57,7 +73,15 @@ def load_trained_model(encoder_path: str,
 def extract_with__and_height(loaded_dict_enc: dict,
                              encoder: networks.ResnetEncoder,
                              device: torch.device) -> tuple[int, int, networks.ResnetEncoder]:
-    """Gets the height and the with of the model.
+    """Gets the height and the with of the model
+
+    Args:
+        loaded_dict_enc (dict): The data loaded
+        encoder (networks.ResnetEncoder): The encoder
+        device (torch.device): The device used
+
+    Returns:
+        tuple[int, int, networks.ResnetEncoder]: The width, height and the encoder
     """
     feed_height = loaded_dict_enc['height']
     feed_width = loaded_dict_enc['width']
@@ -73,7 +97,15 @@ def extract_with__and_height(loaded_dict_enc: dict,
 def load_pretrained_decoder(encoder: networks.ResnetEncoder,
                             depth_decoder_path: str,
                             device: torch.device) -> networks.DepthDecoder:
-    """Loads the pretrained model.
+    """Loads the pretrained model
+
+    Args:
+        encoder (networks.ResnetEncoder): The encoder
+        depth_decoder_path (str): The path to the depth decoder
+        device (torch.device): The device used
+
+    Returns:
+        networks.DepthDecoder: The depth decoder
     """
     depth_decoder = networks.DepthDecoder(
         num_ch_enc=encoder.num_ch_enc, scales=range(4))
@@ -87,7 +119,15 @@ def load_pretrained_decoder(encoder: networks.ResnetEncoder,
 def preproccess_image(frame: Any,
                       feed_width: int,
                       feed_height: int) -> tuple[int, int, transforms.Tensor]:
-    """Preprocesses the image.
+    """Preprocesses the image
+
+    Args:
+        frame (Any): The frame that is being analyzed
+        feed_width (int): The width of the frame
+        feed_height (int): The height of the frame
+
+    Returns:
+        tuple[int, int, transforms.Tensor]: The original width and height and the input image
     """
     frame = pil.fromarray(frame)
     original_width, original_height = frame.size
@@ -102,7 +142,18 @@ def predict_depth(input_image: transforms.Tensor,
                   depth_decoder: networks.DepthDecoder,
                   original_height: int,
                   original_width: int) -> Any:
-    """Predicts the depth of a image.
+    """Predicts the depth of a image
+
+    Args:
+        input_image (transforms.Tensor): The input frame
+        device (torch.device): The device used
+        encoder (networks.ResnetEncoder): The ResnetEncoder
+        depth_decoder (networks.DepthDecoder): The depth decoder
+        original_height (int): The original width
+        original_width (int): The original height
+
+    Returns:
+        Any: The depth of the image
     """
     input_image = input_image.to(device)
     features = encoder(input_image)
@@ -115,7 +166,13 @@ def predict_depth(input_image: transforms.Tensor,
 
 
 def create_colormap_depth_image(disp_resized: Any) -> np.ndarray:
-    """Creates a colormap of the predicted depth.
+    """Creates a colormap of the predicted depth
+
+    Args:
+        disp_resized (Any): The depth of to image
+
+    Returns:
+        np.ndarray: The new image as a np ndarray
     """
     disp_resized_np = disp_resized.squeeze().cpu().numpy()
     vmax = np.percentile(disp_resized_np, 95)
@@ -128,7 +185,13 @@ def create_colormap_depth_image(disp_resized: Any) -> np.ndarray:
 
 
 def display_depth(model_name: str) -> None:
-    """Films and show a preview of the depth.
+    """Films and show a preview of the depth
+
+    Args:
+        model_name (str): The name of the model used
+
+    Raises:
+        SystemError: If no camera is available
     """
     device = get_device()
     encoder_path, depth_decoder_path = download_model(model_name)
